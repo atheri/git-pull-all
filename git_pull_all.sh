99ccbba022a62e86a62b896205141de7352d3228
atheri <corylotze@gmail.com>
2017-06-29 15:20:07 -0700

    Added check to see if the folder is a git repo before trying to pull.

--- git_pull_all.sh
+++ git_pull_all.sh
@@ -2,16 +2,19 @@
 for dir in */
 do
   cols=$(tput cols)
-  message="##### Pulling $dir "
-  remain_n=$(( $cols-${#message} ))
-  
-  fill=""
-  fill_c="#"
-  for (( i=0; i<$remain_n; i++ ))
-  do
-    fill=$fill$fill_c
-  done
- 
-  printf "$message$fill\n"
-  git -C $dir pull
+
+  if git -C $dir rev-parse &> /dev/null; then
+    message="##### Pulling $dir "
+    remain_n=$(( $cols-${#message} ))
+
+    fill=""
+    fill_c="#"
+    for (( i=0; i<$remain_n; i++ ))
+    do
+      fill=$fill$fill_c
+    done
+
+    printf "$message$fill\n"
+    git -C $dir pull
+  fi
 done
