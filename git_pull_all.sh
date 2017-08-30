d079c1cea282ba82bb19b3970c31e9040789b36e
atheri <corylotze@gmail.com>
2017-08-30 13:11:44 -0700

    Added the branch name to the output to help with finding problems. Changed to '-' instead of '#'

--- git_pull_all.sh
+++ git_pull_all.sh
@@ -4,18 +4,19 @@ do
   cols=$(tput cols)
 
   if git -C "$dir" rev-parse &> /dev/null; then
-    message="##### ff merge - $dir "
+    branch_name=$(git -C "$dir" name-rev --name-only HEAD)
+    message="----# $dir$branch_name #"
     remain_n=$(( cols-${#message} ))
 
     fill=""
-    fill_c="#"
+    fill_c="-"
     for (( i=0; i<remain_n; i++ ))
     do
       fill=$fill$fill_c
     done
 
     printf "%s%s\\n" "$message" "$fill"
-    git -C "$dir" merge --ff-only
+    git -C "$dir" pull --ff-only
   fi
 done
 
